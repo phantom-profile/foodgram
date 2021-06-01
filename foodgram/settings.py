ae8ab095e346c56c6d6241212c815c290904d3ca
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-01 14:45:13 +0300

    Update settings.py

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['94.253.85.64', ]
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
