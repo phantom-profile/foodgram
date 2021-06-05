0d8da6f37905b2a384867f3fe5f54feaa4fe3fe9
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-05 18:05:23 +0300

    Update context_processors.py

--- recipes/context_processors.py
+++ recipes/context_processors.py
@@ -2,5 +2,7 @@ from recipes.models import Cart
 
 
 def purchases(request):
-    cart = Cart.objects.get_or_create(owner=request.user)[0]
-    return {'get_purchases_count': cart.recipes.count()}
+    if request.user.is_authenticated:
+        cart = Cart.objects.get_or_create(owner=request.user)[0]
+        return {'get_purchases_count': cart.recipes.count()}
+    return {'get_purchases_count': 0}
